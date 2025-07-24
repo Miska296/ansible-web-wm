cfef746ec8853519251121761a6cac8c0adbe28e
Miska296 <mysak.hanzlikova@seznam.cz>
2025-07-24 15:09:01 +0000

    Přidání export Ansible_roles_path do souboru provision

--- provision.sh
+++ provision.sh
@@ -8,6 +8,9 @@ sudo apt update && sudo apt upgrade -y
 # Instalace závislostí
 sudo apt install -y ansible git ssh
 
+# Nastavit cestu k rolím
+export ANSIBLE_ROLES_PATH="./roles"
+
 # Spuštění playbooku
 ansible-playbook -i inventory/hosts.ini playbooks/webserver.yml
 
